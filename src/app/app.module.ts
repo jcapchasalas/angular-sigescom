82c960a8e145922835ab7f22212ff70b3668dce7
jcapchasalas <jhonkenedy.c@gmail.com>
2020-02-15 15:32:56 -0500

    Fin de la Seccion 6 - @Inputs, @Outputs y ViewChild

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//RUTAS 
+import { APP_ROUTES } from './app.routes';
+
+//MODULOS
+import { PagesModule } from './pages/pages.module';
+
+//TEMPORAL
+import { FormsModule } from '@angular/forms';
+
+//COMPONENTES
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    FormsModule
+  ],
+
+  providers: [],
+  
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
